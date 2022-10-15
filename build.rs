84dfd26ea7f2e9e8218043df315b582c939b8c0c
MostlyWhat <mostlywhatstudios@gmail.com>
2022-10-15 19:36:47 +0700

    Updated Build Script

--- build.rs
+++ build.rs
@@ -1,6 +1,15 @@
 extern crate winres;
+use std::env;
 
 fn main() {
+    // Get current app information
+    let name = env::var("CARGO_PKG_NAME").unwrap();
+    let version = env::var("CARGO_PKG_VERSION").unwrap();
+
+    // Set cargo environment variables for github actions
+    env::set_var("PKG_NAME", name)
+    env::set_var("PKG_VERSION", version);
+
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("icon.ico");
